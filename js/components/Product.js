6c616e22f8a61e5256c62fe1f845af20827e389c
Lucas Chuairi Cruz <lucas.cruz@corp.globo.com>
2018-09-21 18:06:50 -0300

    Trocando card pelo card do material-ui

--- js/components/Product.js
+++ js/components/Product.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import {createFragmentContainer, graphql} from 'react-relay';
 
 class Product extends React.Component {
   render() {
     return (
-      <li className="product">
-        <img
-          className="product__image"
-          src={'/imgs/' + this.props.product.image}></img>
-
-        <strong className="product__name">
-          {this.props.product.name} </strong>
-
-        <span className="product__price">
-          R$ {this.props.product.price.toFixed(2)} </span>
-
-        <Button variant="contained" color="primary">
-          Comprar </Button>
-      </li>
+      <Card className="card">
+        <CardMedia
+          className="card__media"
+          image={'/imgs/' + this.props.product.image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="subheading"
+            className="card__title">
+            {this.props.product.name}
+          </Typography>
+          <Typography component="p">
+            R$ {this.props.product.price.toFixed(2)}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" color="primary">
+            Comprar
+          </Button>
+        </CardActions>
+      </Card>
     );
   }
 }
