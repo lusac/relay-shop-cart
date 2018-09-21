692bbbb65733208400124dbe735fb49331f66b52
Lucas Chuairi Cruz <lucas.cruz@corp.globo.com>
2018-09-21 17:42:09 -0300

    Header do material-ui

--- js/components/Header.js
+++ js/components/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Badge from '@material-ui/core/Badge';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 export default class Header extends React.Component {
   render() {
     return (
-      <header className="header">
-        <h1>Produtos</h1>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit" noWrap>
+            Minha Lojinha
+          </Typography>
+
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     )
   }
 }
