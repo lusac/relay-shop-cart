22a6e43042d4f96c23722190ac91297c25d6c9a8
Lucas Chuairi Cruz <lucas.cruz@corp.globo.com>
2018-09-24 19:27:10 -0300

    Empty state mini cart

--- js/components/MiniCart.js
+++ js/components/MiniCart.js
@@ -38,38 +38,50 @@ export default class MiniCart extends React.Component {
   }
 
   render() {
+    let itemsKeys = Object.keys(this.props.items)
     return (
       <div className={'mini-cart ' + this._handleToggle()}>
         <div className="mini-cart__header">carrinho</div>
         <div className="mini-cart__section">
-          <ul className="mini-cart__items">
-            {Object.keys(this.props.items).map((id) => {
-              let finalPrice = this.props.items[id].qty * this.props.items[id].price
-              return (
-                <li key={id} className="mini-cart__item">
-                  <span className="mini-cart__item__remove" onClick={this._handleRemoveProduct.bind(this, this.props.items[id])}>X</span>
-                  <img
-                    className="mini-cart__item__img"
-                    src={'/imgs/' + this.props.items[id].image}></img>
-                  <span className="mini-cart__item__middle">
-                    <h5 className="mini-cart__item__name">
-                      {this.props.items[id].name}
-                    </h5>
-                    <small className="mini-cart__item__qty">
-                      Quantidade: {this.props.items[id].qty}
-                    </small>
-                    <span className="mini-cart__change-qty">
-                      <span onClick={this._handleIncreaseProduct.bind(this, this.props.items[id])}>aumentar</span>
-                      <span onClick={this._handleDecreaseProduct.bind(this, this.props.items[id])}>diminuir</span>
+          {itemsKeys.length > 0 &&
+            <ul className="mini-cart__items">
+              {itemsKeys.map((id) => {
+                let finalPrice = this.props.items[id].qty * this.props.items[id].price
+                return (
+                  <li key={id} className="mini-cart__item">
+                    <span className="mini-cart__item__remove" onClick={this._handleRemoveProduct.bind(this, this.props.items[id])}>X</span>
+                    <img
+                      className="mini-cart__item__img"
+                      src={'/imgs/' + this.props.items[id].image}></img>
+                    <span className="mini-cart__item__middle">
+                      <h5 className="mini-cart__item__name">
+                        {this.props.items[id].name}
+                      </h5>
+                      <small className="mini-cart__item__qty">
+                        Quantidade: {this.props.items[id].qty}
+                      </small>
+                      <span className="mini-cart__change-qty">
+                        <span onClick={this._handleIncreaseProduct.bind(this, this.props.items[id])}>aumentar</span>
+                        <span onClick={this._handleDecreaseProduct.bind(this, this.props.items[id])}>diminuir</span>
+                      </span>
                     </span>
-                  </span>
-                  <strong className="mini-cart__item__total">
-                    R$ {finalPrice.toFixed(2)}
-                  </strong>
-                </li>
-              )
-            })}
-          </ul>
+                    <strong className="mini-cart__item__total">
+                      R$ {finalPrice.toFixed(2)}
+                    </strong>
+                  </li>
+                )
+              })}
+            </ul>
+          }
+
+          {itemsKeys.length === 0 &&
+            <ul className="mini-cart__items mini-cart__items--empty">
+              <li>
+                <strong className="mini-cart__empty-cart">Carrinho vazio.</strong>
+                <p>Vá as comprar!</p>
+              </li>
+            </ul>
+          }
         </div>
         <div className="mini-cart__footer">
           total R$ {this.props.totalPrice}
