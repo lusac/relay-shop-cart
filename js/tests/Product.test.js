f0303d5a21f719cb567b90c7cad25dc362423da6
Lucas Chuairi Cruz <lucas.cruz@corp.globo.com>
2018-09-24 22:16:36 -0300

    Product: testes unitários

--- js/tests/Product.test.js
+++ js/tests/Product.test.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import {Product} from '../components/Product'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import AddProductToCartMutation from '../mutations/AddProductToCartMutation'
+
+configure({ adapter: new Adapter() })
 
 describe('Product component', () => {
   describe('snapshot', () => {
@@ -11,6 +16,22 @@ describe('Product component', () => {
         },
         renderedValue = renderer.create(<Product product={product} />).toJSON()
       expect(renderedValue).toMatchSnapshot()
+    })
+  })
+
+  describe('methods', () => {
+    describe('method _handleBuyProduct', () => {
+      const relay = {environment:{xunda:1}}
+
+      it('Add product when amount bigger then 0', () => {
+        const product = {
+          id: 1, name: 'item a', image: 'a.png', price: 11, amount: 1
+        }
+        let wrapper = shallow(<Product product={product} relay={relay} />)
+        spyOn(AddProductToCartMutation, 'commit')
+        wrapper.instance()._handleBuyProduct()
+        expect(AddProductToCartMutation.commit).toHaveBeenCalledWith({xunda:1}, product)
+      })
     })
   })
 })
